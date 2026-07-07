954cc394ff05cbedf16cd1b8364e85c2360458a4
Sameer <sameer752591@gmail.com>
2026-07-07 10:54:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+noBtn.addEventListener("touchstart", () => {
+    noBtn.style.position = "absolute";
+    noBtn.style.left = Math.random() * 80 + "%";
+    noBtn.style.top = Math.random() * 80 + "%";
+});
