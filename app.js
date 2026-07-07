0d5200dbc284c79ed0b71ab60383323303f98311
Sameer <sameer752591@gmail.com>
2026-07-07 10:56:44 +0530

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-noBtn.addEventListener("touchstart", () => {
-    noBtn.style.position = "absolute";
-    noBtn.style.left = Math.random() * 80 + "%";
-    noBtn.style.top = Math.random() * 80 + "%";
-});
